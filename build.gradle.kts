cdf23aaff55dc67821efc86db987f5f37c0752fe
ElsaAkhmatyanova <akh24071998@yandex.ru>
2025-10-19 16:56:05 +0500

    configure checkstyle, jacoco, sonar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,10 @@
 plugins {
-	java
+	application
+	checkstyle
+	jacoco
 	id("org.springframework.boot") version "3.5.6"
 	id("io.spring.dependency-management") version "1.1.7"
+	id("org.sonarqube") version "6.2.0.5505"
 }
 
 group = "hexlet.code"
@@ -12,6 +15,10 @@ java {
 	toolchain {
 		languageVersion = JavaLanguageVersion.of(21)
 	}
+}
+
+application {
+	mainClass = "hexlet.code.AppApplication"
 }
 
 configurations {
@@ -36,6 +43,28 @@ dependencies {
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
+checkstyle {
+	toolVersion = "10.3.4"
+	configFile = rootProject.file("config/checkstyle/checkstyle.xml")
+	isIgnoreFailures = false
+}
+
 tasks.withType<Test> {
 	useJUnitPlatform()
+	finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+	reports {
+		xml.required.set(true)
+		html.required.set(true)
+	}
+}
+
+sonar {
+	properties {
+		property("sonar.projectKey", "ElsaAkhmatyanova_java-project-99")
+		property("sonar.organization", "elsaakhmatyanova")
+		property("sonar.host.url", "https://sonarcloud.io")
+	}
 }
